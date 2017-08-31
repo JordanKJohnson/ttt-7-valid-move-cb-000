d71f9322da9471ee33aeb66ff2417add5011a0ca
Jordan Johnson <jkj86@hotmail.com>
2017-08-31 11:15:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,10 +2,12 @@
 def valid_move?(int)
   if int < 0 || int > 8 then false
   else true
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,int)
   if board[int] == "" || board[int] == nil || board[int] == " " then false
   else true
+  end
 end
